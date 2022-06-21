45fd4e000433d244a8d057320494cb78b665b802
Leonardo César <leo_cesa@hotmail.com>
2022-06-21 00:21:41 -0300

    finish first class

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
-import { useState } from 'react'
+import { gql, useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { client } from "./lib/apollo";
 
-function App() {
-  const [count, setCount] = useState(0)
+const GET_QUERY_LESSONS = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
 
-  return (
-    <div className="App">
-      <h1 className="text-red-600 text-2xl font-bold" >Hellow</h1>
-    </div>
-  )
+interface ILessons {
+  id: string
+  title: string
 }
 
-export default App
+function App() {
+  const {data} = useQuery<{lessons: ILessons[]}>(GET_QUERY_LESSONS)
+  // const [lessons, setLessons] = useState([]);
+
+  // const getLessons = async () => {
+  //   try {
+  //     const res = await client.query({
+  //       query: GET_QUERY_LESSONS,
+  //     });
+  //     const data = res.data;
+  //     console.log("data -->", data);
+
+  //     setLessons(data.lessons);
+  //   } catch (e) {
+  //     console.log("error -->", e.message);
+  //   }
+  // };
+
+  // console.log("lessons -->", lessons);
+
+  // useEffect(() => {
+  //   getLessons();
+  // }, []);
+
+  return (
+    <div className="min-h-screen grid place-items-center  ">
+      <ul>
+        {data?.lessons.map((item, index) => {
+          return (
+            <li key={index}>{item.title}</li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
