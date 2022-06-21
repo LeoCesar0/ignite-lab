03e77a9b0bb5748c739b01302962b8c9f89ea69e
Leonardo César <leo_cesa@hotmail.com>
2022-06-20 23:15:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="text-red-600 text-2xl font-bold" >Hellow</h1>
+    </div>
+  )
+}
+
+export default App
