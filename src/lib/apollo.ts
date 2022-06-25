8cfadd21fa51c700cec6a43e6ca0cf5cda3bc009
Leonardo César <leo_cesa@hotmail.com>
2022-06-25 10:57:58 -0300

    update env variables

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ouovs8125j01ywdsx36sra/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers:{
+        'Authorization': `Bearer ${import.meta.env.VITE_SUBSCRIBERS_API_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
